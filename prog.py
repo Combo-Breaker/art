04050bf149b287fa8454665ff7f86a57a3ffe951
Combo-Breaker <impecopeco@gmail.com>
2016-05-02 11:58:27 +0300

    +graph

--- prog.py
+++ prog.py
@@ -5,23 +5,44 @@ import matplotlib.pyplot as plt
 import os
 from scipy import ndimage as ndi
 from skimage.morphology import watershed, disk
-from skimage import data, io
+from skimage import data, io, measure, segmentation
 from skimage.filters import rank
 from skimage.util import img_as_ubyte
 from scipy import misc
 import copy
+from skimage.future import graph
+from skimage.util.colormap import viridis
+from matplotlib import colors
 
 # load the games image
 #img = cv.imread("mem_persist_dali.jpg")
-#img = cv.GaussianBlur(image, (11,11), 0)
-img = io.imread("mem_persist_dali.jpg")
+
+img = io.imread("Raphael_Galatea.jpg")
+img = cv.GaussianBlur(img, (11,11), 0)
 dst = copy.deepcopy(img) 
 
-spatial_radius = 30
+spatial_radius = 40
 color_radius = 30
 cv.pyrMeanShiftFiltering(img, spatial_radius, color_radius, dst) 
 
 denoised = rank.median(dst[:, :, 0], disk(2))
+
+labels = segmentation.slic(img, compactness=15, n_segments=300)
+markers = rank.gradient(denoised, disk(5)) < 10
+markers = ndi.label(markers)[0]
+gradient = rank.gradient(denoised, disk(2))
+#labels = watershed(gradient, markers)
+g = graph.rag_mean_color(img, labels)
+
+cmap = colors.ListedColormap(['#6599FF', '#ff9900'])
+out = graph.draw_rag(labels, g, img, colormap = cmap, thresh = 40)
+
+
+'''
+plt.figure()
+plt.title("RAG with all edges shown in green.")
+plt.imshow(out)
+plt.show()
 
 
 # find continuous region (low gradient -
@@ -36,31 +57,31 @@ gradient = rank.gradient(denoised, disk(2))
 # process the watershed
 labels = watershed(gradient, markers)
 
+#array with size and perimeter of segmets
+properties = measure.regionprops(labels)
+#properties[i].area && properties[i].perimeter
+print(len(properties))
+
+'''
 # display results
-fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(8, 8), sharex=True, sharey=True, subplot_kw={'adjustable':'box-forced'})
+fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(12, 12), sharex=True, sharey=True, subplot_kw={'adjustable':'box-forced'})
 axes = axes.ravel()
-ax0, ax1, ax2, ax3 = axes
+ax0, ax1 = axes
 
-ax0.imshow(img, cmap=plt.cm.gray, interpolation='nearest')
-ax0.set_title("Original")
-#ax1.imshow(gradient, cmap=plt.cm.spectral, interpolation='nearest')
-#ax1.set_title("Local Gradient")
-#ax2.imshow(markers, cmap=plt.cm.spectral, interpolation='nearest')
-#ax2.set_title("Markers")
+ax0.imshow(out, cmap=plt.cm.gray, interpolation='nearest')
+ax0.set_title("Graph")
+ax1.imshow(dst, cmap=plt.cm.spectral, interpolation='nearest', alpha=.9)
+ax1.set_title("Segmented")
+'''
+ax2.imshow(out, cmap=plt.cm.spectral, interpolation='nearest')
+ax2.set_title("Graph")
 ax3.imshow(img, cmap=plt.cm.gray, interpolation='nearest')
-ax3.imshow(labels, cmap=plt.cm.spectral, interpolation='nearest', alpha=.7)
+ax3.imshow(labels, cmap=plt.cm.spectral, interpolation='nearest', alpha=.3)
 ax3.set_title("Segmented")
-
+'''
 for ax in axes:
     ax.axis('off')
 
 fig.tight_layout()
 plt.show()
-
-#cv.imshow("Image", img)
-#cv.waitKey(0)
-
-
-
-
 
